d13d61af399a096f786ae236af682f23b1701620
Anurag Pardeshi <pardeshianurag22@gmail.com>
2025-02-04 11:59:47 +0530

    dashboard and profile responsive

--- frontend/src/Dashboard/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard/Dashboard.jsx
@@ -5,19 +5,17 @@ import { CircleSlider } from "react-circle-slider";
 import Countdown from "react-countdown";
 
 export default function Dashboard() {
-  const [value, setValue] = useState(60); // Initial time in minutes
-  const [timeLeft, setTimeLeft] = useState(value * 60000); // Track remaining time in ms
-  const [isPaused, setIsPaused] = useState(false); // Paused state
-  const [startCountdown, setStartCountdown] = useState(false); // Timer start state
-  const countdownRef = useRef(null); // Reference to countdown
+  const [value, setValue] = useState(60);
+  const [timeLeft, setTimeLeft] = useState(value * 60000);
+  const [isPaused, setIsPaused] = useState(false);
+  const [startCountdown, setStartCountdown] = useState(false);
+  const countdownRef = useRef(null);
 
-  // Handle slider value change
   const handleSliderChange = (newValue) => {
     setValue(newValue);
-    setTimeLeft(newValue * 60000); // Update the time left in milliseconds
+    setTimeLeft(newValue * 60000);
   };
 
-  // Renderer for countdown
   const renderer = ({ hours, minutes, seconds, completed }) => {
     if (completed) {
       return <span>Time is up!</span>;
@@ -31,160 +29,109 @@ export default function Dashboard() {
     }
   };
 
-  // Function to reset the countdown
   const resetTimer = () => {
-    setStartCountdown(false); // Stop countdown
-    setIsPaused(false); // Ensure the countdown isn't paused
-    setValue(60); // Reset slider value to 60 minutes
-    setTimeLeft(60 * 60000); // Reset time left to initial value
+    setStartCountdown(false);
+    setIsPaused(false);
+    setValue(60);
+    setTimeLeft(60 * 60000);
   };
 
   return (
     <>
       <Sidebar />
-      <div className="p-8 sm:ml-64 bg-[#e5e7eb]">
-        <div className=" rounded-lg dark:border-gray-700">
-          <div className="flex justify-between items-center mb-4">
-            <h1 className="text-3xl text-gray-700 font-bold ms-2">Dashboard</h1>
-            <div className="flex">
-              <button
-                type="button"
-                className="bg-white rounded-lg p-2 m-1 flex"
-              >
-                {" "}
+      <div className="p-4 sm:ml-64 bg-[#e5e7eb] min-h-screen">
+        <div className="rounded-lg dark:border-gray-700">
+          <div className="flex flex-col md:flex-row justify-between items-center mb-4">
+            <h1 className="text-3xl text-gray-700 font-bold mb-4 md:mb-0">Dashboard</h1>
+            <div className="flex flex-wrap gap-2">
+              <button className="bg-white rounded-lg p-2 flex items-center gap-2">
                 <BadgeEuro className="text-[#FFAA1D]" /> 1270 coins
               </button>
-              <button
-                type="button"
-                className="bg-white rounded-lg p-2 m-1 flex"
-              >
-                {" "}
+              <button className="bg-white rounded-lg p-2 flex items-center gap-2">
                 <TreePine className="text-[#22c55e]" /> 12 trees planted
               </button>
             </div>
           </div>
-          <div className="grid grid-cols-2 gap-8">
-            <div className="bg-white rounded-lg p-8">
-              <div className="flex justify-between items-center">
-                <h1 className="text-xl text-gray-700 font-semibold">
-                  Focus Timer
-                </h1>
-                <div>
-                  <a
-                    className="text-black bg-gray-200 hover:bg-gray-300 focus:ring-4 focus:ring-blue-300 rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-                    type="button"
-                  >
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+            <div className="bg-white rounded-lg p-6">
+              <div className="flex flex-col sm:flex-row justify-between items-center">
+                <h1 className="text-xl text-gray-700 font-semibold mb-4 sm:mb-0">Focus Timer</h1>
+                <div className="flex flex-wrap gap-2">
+                  <button className="text-black bg-gray-200 hover:bg-gray-300 rounded-lg text-sm px-3 py-2">
                     Add Tags
-                  </a>
-                  <a
-                    type="button"
-                    className="focus:outline-none text-white bg-[#22c55e] hover:bg-[#16a34a] cursor-pointer focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
-                  >
+                  </button>
+                  <button className="text-white bg-[#22c55e] hover:bg-[#16a34a] rounded-lg text-sm px-3 py-2">
                     Start Focus
-                  </a>
+                  </button>
                 </div>
               </div>
+
               <div className="relative flex mt-6 justify-center items-center">
                 <CircleSlider
                   style={{ zIndex: "1000" }}
                   value={value}
                   stepSize={1}
                   onChange={handleSliderChange}
-                  size={250}
-                  max={120} // Maximum 120 minutes
+                  size={200}
+                  max={120}
                   gradientColorFrom="#22c55e"
                   gradientColorTo="#22c55e"
                   knobRadius={10}
-                  circleWidth={20}
+                  circleWidth={15}
                 />
-
-                {/* Countdown Text Overlay */}
                 <div className="absolute text-center">
                   {!startCountdown ? (
-                    <div>
-                      <h2 className="text-[#22c55e] text-4xl font-bold">
-                        {value}
-                      </h2>
+                    <>
+                      <h2 className="text-[#22c55e] text-4xl font-bold">{value}</h2>
                       <h2 className="text-2xl">Minutes</h2>
-                    </div>
+                    </>
                   ) : (
                     <h3 className="text-[#85ce14] text-4xl">
-                      <Countdown
-                        ref={countdownRef}
-                        date={Date.now() + timeLeft}
-                        autoStart={!isPaused}
-                        renderer={renderer}
-                        onComplete={resetTimer}
-                      />
+                      <Countdown ref={countdownRef} date={Date.now() + timeLeft} autoStart={!isPaused} renderer={renderer} onComplete={resetTimer} />
                     </h3>
                   )}
                 </div>
               </div>
             </div>
-            <div className="bg-white rounded-lg p-8">
-              <h1 className="text-xl text-gray-700 font-semibold">
-                Statistics
-              </h1>
-              <div className="grid grid-cols-3 my-4">
-                <div className="text-center">
+
+            <div className="bg-white rounded-lg p-6">
+              <h1 className="text-xl text-gray-700 font-semibold">Statistics</h1>
+              <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 my-4 text-center">
+                <div>
                   <h4 className="text-[#22c55e] text-2xl font-bold">85%</h4>
                   <p>Success Rate</p>
                 </div>
-                <div className="text-center">
+                <div>
                   <h4 className="text-[#3b82f6] text-2xl font-bold">42</h4>
                   <p>Total Sessions</p>
                 </div>
-                <div className="text-center">
+                <div>
                   <h4 className="text-[#a855f7] text-2xl font-bold">26hr</h4>
                   <p>Total Focus Time</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="bg-white rounded-lg p-8 mt-8">
-            <h2 className="text-xl text-gray-700 font-semibold my-6 ps-4">
-              Recent Activities
-            </h2>
-            <div className="gap-4">
-              <div className="flex justify-between items-center bg-gray-50 mb-4 p-4 rounded-lg">
-                <div className="flex items-center">
-                  <Dot size={40} color="green" />
+
+          <div className="bg-white rounded-lg p-6 mt-6">
+            <h2 className="text-xl text-gray-700 font-semibold my-4">Recent Activities</h2>
+            <div className="space-y-4">
+              {[1, 2, 3].map((item, index) => (
+                <div key={index} className="flex flex-wrap justify-between items-center bg-gray-50 p-4 rounded-lg">
+                  <div className="flex items-center gap-2 flex-wrap justify-center">
+                    <Dot size={30} color={index % 2 === 0 ? "green" : "red"} />
+                    <div>
+                      <h2>DSA Problem Solving</h2>
+                      <p>09:00 AM to 10:00 AM</p>
+                    </div>
+                  </div>
                   <div>
-                    <h2>DSA Problem Solving</h2>
-                    <p>09:00 AM to 10:00 AM</p>
+                    <h2>60 Minutes</h2>
+                    <p>+100 coins</p>
                   </div>
                 </div>
-                <div>
-                  <h2>60 Minutues</h2>
-                  <p>+100 coins</p>
-                </div>
-              </div>
-              <div className="flex justify-between items-center bg-gray-50 mb-4 p-4 rounded-lg">
-                <div className="flex items-center">
-                  <Dot size={40} color="red" />
-                  <div>
-                    <h2>DSA Problem Solving</h2>
-                    <p>09:00 AM to 10:00 AM</p>
-                  </div>
-                </div>
-                <div>
-                  <h2>60 Minutues</h2>
-                  <p>+100 coins</p>
-                </div>
-              </div>
-              <div className="flex justify-between items-center bg-gray-50 p-4 rounded-lg">
-                <div className="flex items-center">
-                  <Dot size={40} color="green" />
-                  <div>
-                    <h2>DSA Problem Solving</h2>
-                    <p>09:00 AM to 10:00 AM</p>
-                  </div>
-                </div>
-                <div>
-                  <h2>60 Minutues</h2>
-                  <p>+100 coins</p>
-                </div>
-              </div>
+              ))}
             </div>
           </div>
         </div>
--- frontend/src/Dashboard/Profile/Profile.jsx
+++ frontend/src/Dashboard/Profile/Profile.jsx
@@ -1,81 +1,70 @@
-import React, { useMemo } from "react";
+import React from "react";
 import { UserPen } from "lucide-react";
 import Sidebar from "../Sidebar";
-
-const generateContributions = () => {
-  const contributions = [];
-  const today = new Date();
-  for (let i = 0; i < 365; i++) {
-    const date = new Date(today);
-    date.setDate(date.getDate() - i);
-    contributions.unshift({
-      date,
-      count: Math.floor(Math.random() * 5), // 0-4 contributions
-    });
-  }
-  return contributions;
-};
 
 export default function Profile() {
   return (
     <>
       <Sidebar />
-      <div className="p-8 sm:ml-64 bg-[#e5e7eb] h-full">
+      <div className="p-4 sm:p-8 sm:ml-64 bg-[#e5e7eb] min-h-screen">
         <div className="rounded-lg dark:border-gray-700">
-          <div className="flex flex-wrap justify-between pb-8">
+          <div className="flex flex-wrap justify-between items-center pb-4 sm:pb-8">
             <div>
-              <h1 className="text-3xl text-gray-700 font-bold ms-2">
+              <h1 className="text-2xl sm:text-3xl text-gray-700 font-bold">
                 My Profile
               </h1>
             </div>
             <div>
               <button
                 type="button"
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 sm:px-5 sm:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               >
                 Save Changes
               </button>
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-5 flex-wrap gap-8">
-          <div className="bg-white flex flex-col justify-between rounded-xl col-span-3 shadow-sm p-4">
-            <div className="flex items-center p-8 justify-between">
-              <div className="flex gap-4 items-center">
+
+        <div className="flex flex-col lg:flex-row gap-4 sm:gap-8">
+          {/* Profile Section */}
+          <div className="bg-white rounded-lg shadow-sm flex-1 p-4 sm:p-6">
+            <div className="flex flex-col sm:flex-row items-center justify-between gap-4 p-4 sm:p-6">
+              <div className="flex items-center gap-4">
                 <img
                   src="/avatar.png"
-                  alt=""
-                  className="rounded-lg"
-                  style={{ width: "4rem" }}
+                  alt="Profile"
+                  className="w-12 h-12 sm:w-16 sm:h-16 rounded-lg"
                 />
                 <div>
-                  <p>Anurag Pardeshi</p>
-                  <p>Member since 21 January 2025</p>
+                  <p className="text-lg sm:text-xl font-medium">Anurag Pardeshi</p>
+                  <p className="text-sm sm:text-base text-gray-600">
+                    Member since 21 January 2025
+                  </p>
                 </div>
               </div>
               <div>
-                <a
+                <button
                   type="button"
-                  className="bg-gray-50 text-gray-600 shadow-sm flex gap-2 rounded-lg p-2"
+                  className="bg-gray-50 text-gray-600 shadow-sm flex items-center gap-2 rounded-lg px-3 py-2"
                 >
-                  <UserPen /> Edit
-                </a>
+                  <UserPen size={16} /> Edit
+                </button>
               </div>
             </div>
 
-            <form>
-              <div className="grid gap-6 mb-4 px-6 md:grid-cols-2">
+            <form className="mt-4">
+              <div className="grid gap-4 mb-4 md:grid-cols-2">
                 <div>
                   <label
                     htmlFor="full_name"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Full name
                   </label>
                   <input
                     type="text"
                     id="full_name"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     placeholder="Anurag Pardeshi"
                     required
                   />
@@ -83,117 +72,163 @@ export default function Profile() {
                 <div>
                   <label
                     htmlFor="username"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900"
                   >
-                    username
+                    Username
                   </label>
                   <input
                     type="text"
                     id="username"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     placeholder="anuragpardeshi"
                     required
                   />
                 </div>
-                <div className="relative">
-                  <div>
-                    <label
-                      htmlFor="datepicker-actions"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                    >
-                      DOB
-                    </label>
+                <div>
+                  <label
+                    htmlFor="datepicker-actions"
+                    className="block mb-2 text-sm font-medium text-gray-900"
+                  >
+                    DOB
+                  </label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                      <svg
+                        className="w-4 h-4 text-gray-500"
+                        aria-hidden="true"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                      >
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
+                      </svg>
+                    </div>
+                    <input
+                      id="datepicker-actions"
+                      type="text"
+                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5"
+                      placeholder="Select date"
+                    />
                   </div>
-                  <div className="absolute pt-3 start-0 flex items-center ps-3 pointer-events-none">
-                    <svg
-                      className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                      aria-hidden="true"
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                    >
-                      <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
-                    </svg>
-                  </div>
-                  <input
-                    id="datepicker-actions"
-                    datepicker
-                    datepicker-buttons
-                    datepicker-autoselect-today
-                    type="text"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                    placeholder="Select date"
-                  />
                 </div>
                 <div>
                   <label
                     htmlFor="phone"
-                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    className="block mb-2 text-sm font-medium text-gray-900"
                   >
                     Phone number
                   </label>
                   <input
                     type="tel"
                     id="phone"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                     placeholder="123-45-678"
                     pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
                     required
                   />
                 </div>
               </div>
-              <div className="mb-4 px-6">
+              <div className="mb-4">
                 <label
                   htmlFor="email"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Email address
                 </label>
                 <input
                   type="email"
                   id="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                   placeholder="john.doe@company.com"
                   required
                 />
               </div>
-              <div className="mb-4 px-6">
+              <div className="mb-4">
                 <label
                   htmlFor="bio"
-                  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  className="block mb-2 text-sm font-medium text-gray-900"
                 >
                   Bio
                 </label>
                 <textarea
                   id="bio"
                   rows="4"
-                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
                   placeholder="Write your thoughts here..."
                 ></textarea>
               </div>
             </form>
           </div>
-          <div className="bg-white rounded-xl col-span-2 shadow-sm p-8">
-            <h1>Statistics</h1>
-            <div className="flex justify-between">
-              <p>Current Streak</p>
-              <p>15 days</p>
+
+          {/* Statistics Section */}
+          <div className="flex flex-col gap-4 sm:gap-8 w-full lg:w-1/3">
+            <div className="bg-white rounded-lg shadow-sm p-4 sm:p-6">
+              <h1 className="text-xl text-gray-700 font-semibold mb-4">
+                Statistics
+              </h1>
+              <div className="space-y-3">
+                <div className="flex justify-between">
+                  <p className="text-gray-600">Current Streak</p>
+                  <p className="text-gray-800 font-bold">15 days</p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="text-gray-600">Longest Streak</p>
+                  <p className="text-gray-800 font-bold">21 days</p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="text-gray-600">Total Focus Time</p>
+                  <p className="text-gray-800 font-bold">126 hours</p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="text-gray-600">Trees Planted</p>
+                  <p className="text-gray-800 font-bold">24 trees</p>
+                </div>
+                <div className="flex justify-between">
+                  <p className="text-gray-600">Success Rate</p>
+                  <p className="text-gray-800 font-bold">85%</p>
+                </div>
+              </div>
             </div>
-            <div className="flex justify-between">
-              <p>Longest Streak</p>
-              <p>21 days</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Total Focus Time</p>
-              <p>126 hours</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Trees Planted</p>
-              <p>24 trees</p>
-            </div>
-            <div className="flex justify-between">
-              <p>Success Rate</p>
-              <p>85%</p>
+
+            <div className="bg-white rounded-lg shadow-sm p-4 sm:p-6">
+              <h1 className="text-xl text-gray-700 font-semibold mb-4">
+                Achievements
+              </h1>
+              <div className="space-y-4">
+                <div className="bg-gray-50 flex items-center gap-4 p-4 rounded-lg">
+                  <img src="/trophy.png" className="w-8 h-8" alt="Trophy" />
+                  <div>
+                    <p className="text-sm sm:text-base font-medium">
+                      21-Day Streak
+                    </p>
+                    <p className="text-xs sm:text-sm text-gray-600">
+                      Maintained focus for 21 consecutive days
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-gray-50 flex items-center gap-4 p-4 rounded-lg">
+                  <img src="/trophy.png" className="w-8 h-8" alt="Trophy" />
+                  <div>
+                    <p className="text-sm sm:text-base font-medium">
+                      Forest Guardian
+                    </p>
+                    <p className="text-xs sm:text-sm text-gray-600">
+                      Maintained focus for 21 consecutive days
+                    </p>
+                  </div>
+                </div>
+                <div className="bg-gray-50 flex items-center gap-4 p-4 rounded-lg">
+                  <img src="/trophy.png" className="w-8 h-8" alt="Trophy" />
+                  <div>
+                    <p className="text-sm sm:text-base font-medium">
+                      21-Day Streak
+                    </p>
+                    <p className="text-xs sm:text-sm text-gray-600">
+                      Maintained focus for 21 consecutive days
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
